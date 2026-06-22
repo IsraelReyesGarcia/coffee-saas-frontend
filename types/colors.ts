2f9e4122de47dee5e758e2e190eb08e2833fdc4a
IsraelReyesGarcia <li_israelreyes@unca.edu.mx>
2026-06-21 23:34:12 -0600

    feat: Se creó el directorio types para el tipado del proyecto

---
+++ types/colors.ts
@@ -0,0 +1,10 @@
+export enum TableColors {
+    libre='#82B4A2',
+    Ocupada='#5A262C'
+}
+
+export enum ProgressColor{
+    PENDIENTE = '#484A6E',
+    PREPARANDO = '#DBA400',
+    LISTO = '#ADEBB3'
+}
